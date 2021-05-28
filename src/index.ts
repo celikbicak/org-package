31ddd4cd00787e87d2dc612e1c5865f76bec3e7e
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2021-05-28 17:16:34 +0300

    Add function and format code

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { enum2array } from "enum2array";
-
+import { enum2array } from 'enum2array';
 
 enum CustomType {
-    TypeA = 1,
-    TypeB = 2,
-    TypeC = 3,
-    TypeD = 4,
+  TypeA = 1,
+  TypeB = 2,
+  TypeC = 3,
+  TypeD = 4,
 }
 
 enum CustomValue {
-    TypeA = "valueA",
-    TypeB = "valueB",
-    TypeC = "valueC",
-    TypeD = "valueD",
+  TypeA = 'valueA',
+  TypeB = 'valueB',
+  TypeC = 'valueC',
+  TypeD = 'valueD',
 }
 
+export function orgPackage(): void {
+  console.log(enum2array(CustomType));
 
-console.log(enum2array(CustomType));
-
-console.log(enum2array(CustomValue));
+  console.log(enum2array(CustomValue));
+}
